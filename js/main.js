9bc4cb4b26b5e3e3b343bdb8f24714a87604ad4b
Michelle Li <michelle.li.862@gmail.com>
2019-08-12 11:41:18 -0700

    Updated name for resume file

--- js/main.js
+++ js/main.js
@@ -63,15 +63,15 @@ $(function() {
 
   window.sr = ScrollReveal({
     reset: false,
-    duration: 600,
+    duration: 400,
     easing: 'cubic-bezier(.694,0,.335,1)',
     scale: 1,
-    viewFactor: 0.3,
+    viewFactor: 0.1,
   });
 
   // sr.reveal('.background');
-  sr.reveal('.skills');
-  sr.reveal('.experience', { viewFactor: 0.2 });
+  sr.reveal('.skills', { viewFactor: 0.1 });
+  sr.reveal('.experience', { viewFactor: 0.1 });
   sr.reveal('.featured-projects', { viewFactor: 0.1 });
-  sr.reveal('.other-projects', { viewFactor: 0.05 });
+  sr.reveal('.other-projects', { viewFactor: 0.1 });
 });
